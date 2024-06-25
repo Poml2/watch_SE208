3bf3202574f73c02e94c44547dfe34561fee4c51
Keshav Das <68956570+Waysox@users.noreply.github.com>
2024-06-25 23:02:50 +0200

    Update HeartRateFFT.py
    Correction après les discussions de mardi 25/06

--- code/TP_ADC_RasPico/files/HeartRateFFT.py
+++ code/TP_ADC_RasPico/files/HeartRateFFT.py
@@ -15,42 +15,57 @@ def HeartRateFFT(windowed_signal, Fe):
 
     return(60*freq[np.argmax(spectre)])
 
-""" # POUR TESTER LE FONCTIONNEMENT AVEC LES DONNEES DE TEST
-import csv
-import matplotlib.pyplot as plt
-import numpy as np
 
-file = open("   METTRE CHEMIN ADEQUAT ICI   /TP_ADC_RasPico/files/HeartAcq_mod.csv")
+"""
+# POUR TESTER LE FONCTIONNEMENT AVEC LES DONNEES DE TEST CSV
+import matplotlib.pyplot as plt
+import csv
+
+file = open("files/HeartAcq_mod.csv")
 csv_reader = csv.reader(file)
-rows = []
-#Lecture des données dans la liste rows
+signal = []
+#Lecture des données dans la liste signal
 for row in csv_reader:
-    rows.append(row)
+    signal.append(row)
 file.close()
 
 #On enlève les informations du début du fichier (nom de l'oscillo, etc.)
-rows = rows[10:]
+signal = signal[10:]
 
 #On met les string sous forme de nombre réels
-for i in range(len(rows)):
-    rows[i] = [float(rows[i][0]), float(rows[i][1])]
+for i in range(len(signal)):
+    signal[i] = float(signal[i][1])
+"""
 
-#Conversion en array pour des manipulations plus faciles
-rows = np.asarray(rows)
+"""
+# POUR TESTER LE FONCTIONNEMENT AVEC LES DONNEES DE TEST ENREGISTREES
+import matplotlib.pyplot as plt
+
+file = open("files/out_10s.txt")
+signal = []
+#Lecture des données dans la liste rows
+for row in file:
+    if row != ',\n':
+        signal.append(float(row))
+file.close()
+
+
 
 #Fréquence d'échantillonage (80Hz dans l'exemple du fichier HeartAcq_mod.csv)
 Fe = 80
 #Calcul du spectre sur window_size échantillons soit window_size/Fe = ... secondes
 window_size = 400
 
-spectre = np.abs(np.fft.rfft(rows[:window_size,1]))
+spectre = np.abs(np.fft.rfft(signal[:window_size]))
 #Array des fréquences du spectre
-freq = np.fft.rfftfreq(len(rows[:window_size]), d=1/Fe)
+freq = np.fft.rfftfreq(len(signal[:window_size]), d=1/Fe)
 
 #La composante continue (spectre[0])
 #ne nous intéresse pas donc elle est retirée
-freq = freq[1:]
-spectre = spectre[1:]
+#tout comme les composantes au-dessus de 4 Hz qui sont forcément du bruit
+upper_bound = int(round(4*len(freq)/freq[-1]))
+freq = freq[1: upper_bound]
+spectre = spectre[1: upper_bound]
 
 plt.plot(freq, spectre)
 plt.title("Spectre du signal du capteur sans composante continue (calculé ici sur " + str(window_size) + "/" + str(Fe) + " = "+ str(window_size/Fe) +" secondes)")
@@ -58,6 +73,6 @@ plt.xlabel("Battements par seconde")
 plt.ylabel("Amplitude en unités arbitraires")
 
 #Rythme cardiaque
-print(60*freq[np.argmax(spectre)])
+print("Fréquence cardiaque :", 60*freq[np.argmax(spectre)])
 plt.show()
 """
